ffd64857a71281e34287a43786730ad3206b35b3
Sp1302 <43338163+Sp1302@users.noreply.github.com>
2019-02-21 14:41:55 +0530

    Create 36.c

---
+++ 36.c
@@ -0,0 +1,22 @@
+# set-4
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char str[50],i,c=0;
+printf("\nenter the word");
+gets(str);
+for(i=0;str[i]!='\0';i++)
+{
+if((str[i]>='0')&&(str[i]<='9')||(str[i]>='a')&&(str[i]<='z'))
+{
+continue;
+}
+else
+{
+c++;
+}
+}
+printf("%d\n",c);
+return 0;
+}
