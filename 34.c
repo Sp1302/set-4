0df48559bc10e190645a12a79781fd03aa10c9fd
Sp1302 <43338163+Sp1302@users.noreply.github.com>
2019-02-21 14:30:54 +0530

    Create 34.c

---
+++ 34.c
@@ -0,0 +1,18 @@
+# set-4
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char str[50],i,c=0;
+printf("\nenter the word");
+gets(str);
+for(i=0;str[i]!='\0';i++)
+{
+if(str[i]=='.')
+{
+c++;
+}
+}
+printf("%d\n",c+1);
+return 0;
+}
